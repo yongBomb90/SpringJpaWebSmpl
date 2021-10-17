93f3e41677dda50bdc1aea9684d3449e8d1a3dff
yongBomb <yongBomb90@gmail.com>
2021-10-17 21:09:48 +0900

    회원가입2강 - 계정도메인

---
+++ src/main/java/com/bomb/springjpasmpl/domain/Account.java
@@ -0,0 +1,55 @@
+package com.bomb.springjpasmpl.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "Id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private LocalDateTime joinedAt;
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+
+    @Lob @Basic(fetch = FetchType.LAZY)
+    private String profileImage;
+
+    private boolean studyCreatedByEmail;
+
+    private boolean studyCreatedByWeb;
+
+    private boolean studyEnrollmentResultByEmail;
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdatedByEmail;
+
+    private boolean studyUpdatedByWeb;
+
+
+}
