3c701777675b3d23665f3d1ef9d22b6e9c241de1
yongBomb <yongBomb90@gmail.com>
2021-10-17 22:12:47 +0900

    회원가입4강 - 회원가입뷰

---
+++ src/main/java/com/bomb/springjpasmpl/accout/SignUpForm.java
@@ -0,0 +1,20 @@
+package com.bomb.springjpasmpl.accout;
+
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+public class SignUpForm {
+
+    private String nickname;
+
+    private String email;
+
+    private String password;
+
+
+
+
+}
--- src/test/java/com/bomb/springjpasmpl/account/AccountControllerTest.java
+++ src/test/java/com/bomb/springjpasmpl/account/AccountControllerTest.java
@@ -6,11 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder.*;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @SpringBootTest
@@ -25,7 +23,10 @@ public class AccountControllerTest {
     void signUpForm() throws Exception {
         mockMvc.perform(get("/sign-up"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("account/sign-up"));
+                .andExpect(view().name("account/sign-up"))
+                .andExpect(model().attributeExists("signUpForm"))
+
+        ;
     }
 
 
