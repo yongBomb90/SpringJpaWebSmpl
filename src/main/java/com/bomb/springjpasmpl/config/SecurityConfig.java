e6045be1e15af65a7633fc8851f8a8f13df58897
yongBomb <yongBomb90@gmail.com>
2021-10-20 21:17:04 +0900

    회원가입16강 - 가입 확인 이메일 재전송

--- src/main/java/com/bomb/springjpasmpl/config/SecurityConfig.java
+++ src/main/java/com/bomb/springjpasmpl/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         //super.configure(http);
         http.authorizeRequests()
-                .mvcMatchers("/","/login","/sign-up","/check-email","/check-email-token","/email-login","/check-email-login","/login-link").permitAll()
+                .mvcMatchers("/","/login","/sign-up","/check-email-token","/email-login","/check-email-login","/login-link").permitAll()
                 .mvcMatchers(HttpMethod.GET,"/profile/*").permitAll()
                 .anyRequest().authenticated();
     }
--- src/main/java/com/bomb/springjpasmpl/domain/Account.java
+++ src/main/java/com/bomb/springjpasmpl/domain/Account.java
@@ -27,6 +27,8 @@ public class Account {
 
     private String emailCheckToken;
 
+    private LocalDateTime emailCheckTokenGeneratedAt;
+
     private LocalDateTime joinedAt;
 
     private String bio;
@@ -54,6 +56,7 @@ public class Account {
 
     public void generateEmailCheckToken() {
         this.emailCheckToken = UUID.randomUUID().toString();
+        this.emailCheckTokenGeneratedAt = LocalDateTime.now();
     }
 
     public void completeSignUp() {
@@ -63,6 +66,10 @@ public class Account {
 
     public boolean isValidToken(String token) {
         return this.emailCheckToken.equals(token);
+    }
+
+    public boolean canSendConfirmEmail() {
+        return this.emailCheckTokenGeneratedAt.isBefore(LocalDateTime.now().minusHours(1));
     }
 
 }
