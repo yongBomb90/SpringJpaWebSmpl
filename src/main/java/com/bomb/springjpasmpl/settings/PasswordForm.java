999bfd001a4a9087605f6d99abfccac45ea345f8
yongBomb <yongBomb90@gmail.com>
2021-10-23 21:46:14 +0900

    회원가입24강 - 패스워드 수정

---
+++ src/main/java/com/bomb/springjpasmpl/settings/PasswordForm.java
@@ -0,0 +1,15 @@
+package com.bomb.springjpasmpl.settings;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+@Data
+public class PasswordForm {
+
+    @Length(min = 8, max = 50)
+    private String newPassword;
+
+    @Length(min = 8, max = 50)
+    private String newPasswordConfirm;
+
+}
