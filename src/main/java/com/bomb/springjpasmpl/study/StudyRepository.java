1ca937e183592d7d8c80e6a427ca0b0dde112a72
yongBomb <yongBomb90@gmail.com>
2021-10-31 21:27:46 +0900

    스터디1강 - 스터디 개설 view summer-note

---
+++ src/main/java/com/bomb/springjpasmpl/study/StudyRepository.java
@@ -0,0 +1,13 @@
+package com.bomb.springjpasmpl.study;
+
+
+import com.bomb.springjpasmpl.domain.Study;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface StudyRepository extends JpaRepository<Study, Long> {
+
+    boolean existsByPath(String path);
+
+}
---
+++ src/main/java/com/bomb/springjpasmpl/study/StudyService.java
@@ -0,0 +1,22 @@
+package com.bomb.springjpasmpl.study;
+
+
+import com.bomb.springjpasmpl.domain.Account;
+import com.bomb.springjpasmpl.domain.Study;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class StudyService {
+
+    private final StudyRepository repository;
+
+    public Study createNewStudy(Study study, Account account) {
+        Study newStudy = repository.save(study);
+        newStudy.addManager(account);
+        return newStudy;
+    }
+}
