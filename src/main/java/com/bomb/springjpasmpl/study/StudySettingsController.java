19d8b737bb948b8b17bc5adfb603e330553b7e42
yongBomb <yongBomb90@gmail.com>
2021-11-01 21:38:03 +0900

    스터디5강 - 스터디 설명 수정처리

---
+++ src/main/java/com/bomb/springjpasmpl/study/StudySettingsController.java
@@ -0,0 +1,61 @@
+package com.bomb.springjpasmpl.study;
+
+import com.bomb.springjpasmpl.accout.CurrentAccount;
+import com.bomb.springjpasmpl.domain.Account;
+import com.bomb.springjpasmpl.domain.Study;
+import com.bomb.springjpasmpl.study.form.StudyDescriptionForm;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+@Controller
+@RequestMapping("/study/{path}/settings")
+@RequiredArgsConstructor
+public class StudySettingsController {
+
+    private final StudyService studyService;
+    private final ModelMapper modelMapper;
+
+    @GetMapping("/description")
+    public String viewStudySetting(@CurrentAccount Account account, @PathVariable String path, Model model) {
+        Study study = studyService.getStudyToUpdate(account, path);
+        model.addAttribute(account);
+        model.addAttribute(study);
+        model.addAttribute(modelMapper.map(study, StudyDescriptionForm.class));
+        return "study/settings/description";
+    }
+
+    @PostMapping("/description")
+    public String updateStudyInfo(@CurrentAccount Account account, @PathVariable String path,
+                                  @Valid StudyDescriptionForm studyDescriptionForm, Errors errors,
+                                  Model model, RedirectAttributes attributes) {
+        Study study = studyService.getStudyToUpdate(account, path);
+
+        if (errors.hasErrors()) {
+            model.addAttribute(account);
+            model.addAttribute(study);
+            return "study/settings/description";
+        }
+
+        studyService.updateStudyDescription(study, studyDescriptionForm);
+        attributes.addFlashAttribute("message", "스터디 소개를 수정했습니다.");
+        return "redirect:/study/" + getPath(path) + "/settings/description";
+    }
+
+    private String getPath(String path) {
+        return URLEncoder.encode(path, StandardCharsets.UTF_8);
+    }
+
+
+}
