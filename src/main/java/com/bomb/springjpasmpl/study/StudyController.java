78b10f5f83f3ba46861939f606278fd2f346824c
yongBomb <yongBomb90@gmail.com>
2021-11-01 21:20:30 +0900

    스터디2강 - 스터디 조회

--- src/main/java/com/bomb/springjpasmpl/study/StudyController.java
+++ src/main/java/com/bomb/springjpasmpl/study/StudyController.java
@@ -13,6 +13,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -23,6 +24,7 @@ import java.nio.charset.StandardCharsets;
 @RequiredArgsConstructor
 public class StudyController {
 
+    private final StudyRepository studyRepository;
     private final StudyService studyService;
     private final ModelMapper modelMapper;
     private final StudyFormValidator studyFormValidator;
@@ -30,6 +32,13 @@ public class StudyController {
     @InitBinder("studyForm")
     public void studyFormInitBinder(WebDataBinder webDataBinder) {
         webDataBinder.addValidators(studyFormValidator);
+    }
+
+    @GetMapping("/study/{path}")
+    public String viewStudy(@CurrentAccount Account account, @PathVariable String path, Model model) {
+        model.addAttribute(account);
+        model.addAttribute(studyRepository.findByPath(path));
+        return "study/view";
     }
 
     @GetMapping("/new-study")
--- src/main/java/com/bomb/springjpasmpl/study/StudyRepository.java
+++ src/main/java/com/bomb/springjpasmpl/study/StudyRepository.java
@@ -2,6 +2,7 @@ package com.bomb.springjpasmpl.study;
 
 
 import com.bomb.springjpasmpl.domain.Study;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,5 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 public interface StudyRepository extends JpaRepository<Study, Long> {
 
     boolean existsByPath(String path);
+
+    @EntityGraph(value = "Study.withAll", type = EntityGraph.EntityGraphType.LOAD)
+    Study findByPath(String path);
 
 }
