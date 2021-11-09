b8661145c89a709826074bd83f2cfcc3a134bc06
yongBomb <yongBomb90@gmail.com>
2021-11-09 22:50:52 +0900

    단어장 프로젝트 초기 생성

---
+++ src/main/java/com/mtbx/prgword/PrgwordApplication.java
@@ -0,0 +1,13 @@
+package com.mtbx.prgword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrgwordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrgwordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mtbx/prgword/PrgwordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtbx.prgword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrgwordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
