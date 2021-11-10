d38d7d7a1dfee70bf86c5cf93c56c6b5273f5c80
yongBomb <79437777+yongBomb90@users.noreply.github.com>
2021-11-10 21:53:12 +0900

    Update PrgwordApplicationTests.java

--- src/test/java/com/mtbx/prgword/PrgwordApplicationTests.java
+++ src/test/java/com/mtbx/prgword/PrgwordApplicationTests.java
@@ -2,7 +2,9 @@ package com.mtbx.prgword;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+/**
+테스트 메인
+**/
 @SpringBootTest
 class PrgwordApplicationTests {
 
