3ccfee68f712e280579e674c4eb50614f04cba00
yongBomb <yongBomb90@gmail.com>
2021-11-11 21:40:10 +0900

    디렉토리 구상

---
+++ src/main/java/com/mtbx/prgword/infra/conf/AuthConfig.java
@@ -0,0 +1,21 @@
+package com.mtbx.prgword.infra.conf;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 스프링 시큐리티 설정
+ */
+@Configuration
+public class AuthConfig extends WebSecurityConfigurerAdapter {
+
+
+
+
+
+
+
+
+
+}
